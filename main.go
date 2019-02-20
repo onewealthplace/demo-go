4904d1fea25aa721449908604fc6e3a25476699c
Cedric Hauber <hauber.c@gmail.com>
2019-02-20 14:26:11 +0100

    Real webservice

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-  fmt.Println("Hello")
+  r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "world",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
-
